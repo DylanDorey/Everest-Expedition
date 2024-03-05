5ee8e3c714eac204d0649d034deceb9502baccd2
Kyle-Ramser <kyleram03@gmail.com>
2024-03-05 00:02:43 -0800

    Joints Suck
    I'm gonna do more research to see if i can get this thing to work but oml

--- Everest Expedition/Assets/Scripts/CharacterMove.cs
+++ Everest Expedition/Assets/Scripts/CharacterMove.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour
 {
+    public GameObject playerBody;
+    public GameObject hand;
+
+    public Vector3 bodyPos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,23 @@ public class CharacterMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos2D = Input.mousePosition;
+
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 mouseDelta = mousePos3D - bodyPos;
+
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+
+        hand.transform.position = mouseDelta;
+
     }
+
 }
