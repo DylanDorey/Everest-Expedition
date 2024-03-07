0c0617768cba5db42d2d67caab4484e2b85cdfcc
Kyle-Ramser <kyleram03@gmail.com>
2024-03-06 16:57:03 -0800

    Hammer change / forgot to push this

--- Everest Expedition/Assets/Scripts/CharacterMove.cs
+++ Everest Expedition/Assets/Scripts/CharacterMove.cs
@@ -1,40 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class CharacterMove : MonoBehaviour
 {
     public GameObject playerBody;
-    public GameObject hand;
+    public GameObject mousePoint;
+    public GameObject Pick;
 
     public Vector3 bodyPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos2D = Input.mousePosition;
+       Vector3 mousePos2D = Input.mousePosition;
 
-        mousePos2D.z = -Camera.main.transform.position.z;
-        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+       mousePos2D.z = -Camera.main.transform.position.z;
+       Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
 
-        Vector3 mouseDelta = mousePos3D - bodyPos;
-
-        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
-        if (mouseDelta.magnitude > maxMagnitude)
-        {
-            mouseDelta.Normalize();
-            mouseDelta *= maxMagnitude;
-        }
+       Vector3 mouseDelta = mousePos3D - bodyPos;
 
 
-        hand.transform.position = mouseDelta;
+       mousePoint.transform.position = mouseDelta;
 
+       float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+       if (mouseDelta.magnitude > maxMagnitude)
+       {
+           mouseDelta.Normalize();
+           mouseDelta *= maxMagnitude;
+       }
+
+        Pick.transform.position = mouseDelta;
     }
-
 }
