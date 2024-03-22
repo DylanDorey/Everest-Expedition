b411f2058e7c13378a17dbbebdbe3f3211acd37d
idiazjr <idiazjr@csuchico.edu>
2024-03-22 12:38:51 -0700

    Previous save on checkpoints

--- Everest Expedition/Assets/Scripts/Checkpoints.cs
+++ Everest Expedition/Assets/Scripts/Checkpoints.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class Checkpoints : MonoBehaviour
 {
+
     [SerializeField] List<GameObject> Check;
     [SerializeField] Vector3 Playerpoint;
+    public float threshold;
+    public Vector3 vectorpoint;
 
+    private void Update()
+    {
+        if(transform.position.y < threshold)
+        {
+            transform.position = new Vector3(Playerpoint.x,Playerpoint.y,Playerpoint.z);
+        }
+        
+    }
     private void OnTriggerEnter(Collider other)
     {
-
+        vectorpoint = other.transform.position;
+        Destroy(other.gameObject);
     }
 }
