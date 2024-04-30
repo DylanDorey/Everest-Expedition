23cf2fbb0cd0cdb09fa0f8ca653ba6b46ec1c565
Kyle-Ramser <kyleram03@gmail.com>
2024-04-30 09:45:26 -0700

    Commented out code
    commented out bird and joint control, removed the test folder

--- Everest Expedition/Assets/Scripts/Bird.cs
+++ Everest Expedition/Assets/Scripts/Bird.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    //Vector3s for the start and end flight path
     public Vector3 startPoint;
     public Vector3 endPoint;
+
+    //the speed the bird is moving and the time that it waits before flying again
     public float moveSpeed = 5f;
     public float waitTime = 2f;
 
+    //keeps track of the distance that the bird can travel
     [Range(1f, 20f)]
     public float moveDistance;
 
@@ -16,13 +20,17 @@ public class Bird : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //changes the birds start position
         startPoint = transform.position;
+
+        //sets the birds end location
         endPoint = new Vector3(transform.position.x - moveDistance, transform.position.y, transform.position.z);
 
+        //begins the coroutine for moving the bird from start to end
         StartCoroutine(MoveBetweenPoint());
     }
 
-    // Update is called once per frame
+    //moves the bird between a start point and end point
     IEnumerator MoveBetweenPoint()
     {
         while (true)
